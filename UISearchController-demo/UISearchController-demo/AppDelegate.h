125bfcdcd7faa52783900720231a41afd51c93ac
Arvin <yasir86@126.com>
2016-10-18 21:27:24 +0800

    init commit

---
+++ UISearchController-demo/UISearchController-demo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  UISearchController-demo
+//
+//  Created by Arvin on 16/10/18.
+//  Copyright © 2016年 Arvin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ UISearchController-demo/UISearchController-demo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  UISearchController-demo
+//
+//  Created by Arvin on 16/10/18.
+//  Copyright © 2016年 Arvin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
